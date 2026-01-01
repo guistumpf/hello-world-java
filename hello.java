ab018efa56e5774e62972fbbf2c33ada59b44f0e
Joao Fernandes <guist4@proton.me>
2026-01-01 11:58:52 -0300

    testando a declaraçao de variaveis

--- hello.java
+++ hello.java
@@ -1,6 +1,9 @@
 public class hello {
     public static void main(String[] args) {
+        int idade, ano, dia;
+        idade = 27; ano = 2026; dia = 1;
         String nome = "Luis";
-        System.out.println(2025 + 1 + " is here " + nome + "!");
+        System.out.println(nome + " você tem " + idade + " anos de idade");
+        System.out.println("Hoje é dia " + dia + " de janeiro de " + ano);
     }
 }
