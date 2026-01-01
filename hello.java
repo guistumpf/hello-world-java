a28f1517b880966356071276de025d41f03b02a5
Joao Fernandes <guist4@proton.me>
2026-01-01 19:07:06 -0300

    aprendendo while e loop

--- hello.java
+++ hello.java
@@ -1,21 +1,16 @@
 public class hello {
     public static void main(String[] args) {
 
-int idade = 18;
-boolean cidadao = false;
+int x = 3;
 
-if(idade >= 18){
-    System.out.println("Idade suficiente para votar. ");
-    if(cidadao){
-        System.out.println("E você é um cidadão, então pode votar!");
-    }else{
-        System.out.println("Porém você não é um cidadão local.");
-    } 
 
-} else {
-    System.out.println("Idade insuficiente.");
+while (x > 0) {
+    System.out.println(x);
+    x--;
+
 }
 
+System.out.println("Feliz 2026!");
 
 } 
 }
